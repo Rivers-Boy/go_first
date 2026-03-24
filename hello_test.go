60bcecd0c9b61782ea55a5f8a304364f689a7b7c
wanghaijiang <wanghaijiang@xiaomi.com>
2026-03-25 00:32:42 +0800

    add test func and use helper

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("River")
-	want := "Hello, River"
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// 当测试失败时所报告的行号将在函数调用中而不是在辅助函数内部，即打印 24行而不是 11行
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("River")
+		want := "Hello, River"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("world")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
